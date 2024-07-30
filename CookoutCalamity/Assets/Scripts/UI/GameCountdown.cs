681a6739be392b4482047977f0b7345098085bc3
Jonathan <jondmjr@gmail.com>
2024-07-30 15:12:35 -0700

    Revert "Countdown"
    This reverts commit b759177b388e1259dd44557f328200c7a9127678.

--- CookoutCalamity/Assets/Scripts/UI/GameCountdown.cs
+++ CookoutCalamity/Assets/Scripts/UI/GameCountdown.cs
@@ -14,8 +14,6 @@ public class GameCountdown : MonoBehaviour
     private PTableInteract currentProgress_script;
     public float delay = 0.5f;
     private float timer;
-    public bool hasPlayed = false;
-    public AudioClip countdown;
 
     void Start()
     {
@@ -38,21 +36,12 @@ public class GameCountdown : MonoBehaviour
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}: {1:00}", minutes, seconds);
-        
+
         if (remainingTime > 0)
         {
-                if (remainingTime <= 21 && hasPlayed == false)
-                    {
-                    AudioSource scriptedCountdown;
-                    Debug.Log("Start countdown");
-                    scriptedCountdown = GetComponent<AudioSource>();
-                    scriptedCountdown.Play();
-                    hasPlayed = true;
-                    }
-            remainingTime -= Time.deltaTime;       
+            remainingTime -= Time.deltaTime;
         }
         //remainingTime <= 0 && currentProgress_script.currentProgress < 90
-
         else if (remainingTime <= 0 && progressBar.value < 90)
         {
             Debug.Log("IN THIS " + remainingTime);
