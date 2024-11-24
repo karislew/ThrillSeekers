1b83b6a2654a5781df138c0bb9c1a0fcde825695
karislew <157058967+karislew@users.noreply.github.com>
2024-11-23 20:20:46 -0500

    Table State Audio
    The progress of the table bar now plays different stages of the audio

---
+++ CookoutCalamity/Assets/Scripts/NewTestScripts/TableSound.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TableSound : MonoBehaviour
+{
+   public AudioClip[] tableSounds;
+   private AudioSource audioSource;
+   public Slider progressBar;
+   // Start is called before the first frame update
+   float[] thresholds = {0,45,65};
+   private int currentSoundState = -1;
+   void Start()
+   {
+       audioSource = GetComponent<AudioSource>();
+       UpdateSoundState();
+       audioSource.loop=true;
+   }
+
+
+   // Update is called once per frame
+   void Update()
+   {
+       UpdateSoundState();
+   }
+   void UpdateSoundState()
+   {
+       for(int i = thresholds.Length-1; i >= 0;i--)
+       {
+           if(progressBar.value>=thresholds[i])
+           {
+               if(currentSoundState!=i)
+               {
+                   currentSoundState = i;
+                   SetSoundState(i);
+               }
+               break;
+           }
+       }
+   }
+   void SetSoundState(int activeSound)
+   {
+       if(activeSound<0 || activeSound>=tableSounds.Length) return;
+       Debug.Log("SOUND SOUND "+activeSound + " " + tableSounds[activeSound]);
+
+
+       audioSource.clip  = tableSounds[activeSound];
+       if(!audioSource.isPlaying || audioSource.clip!=tableSounds[activeSound])
+       {
+           audioSource.Play();
+       }
+      
+   }
+}
+
+
+
